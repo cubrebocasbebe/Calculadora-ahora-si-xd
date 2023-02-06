d4f34a2d276649f37265e3dbde3e99ecc9db7f8d
cubrebocasbebe <112137955+cubrebocasbebe@users.noreply.github.com>
2023-02-05 22:25:10 -0600

    Add files via upload

---
+++ lidia/src/main/java/com/mycompany/lidia/Lidia.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lidia;
+
+/**
+ *
+ * @author Jolette
+ */
+public class Lidia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        iniciar obj=new iniciar();
+        obj.show(true);
+    }
+}
